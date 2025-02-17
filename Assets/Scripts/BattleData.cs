ce5196587789cfbfd0200f538312c05a20a2ab80
진재환 <jinjh99@gmail.com>
2025-02-17 14:58:40 +0900

    first

---
+++ Assets/Scripts/BattleData.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct battleData
+{
+    public int attackPower;
+    public int hp;
+    public int MaxHp;
+    public float attackSpeed;
+    public int cost;
+    public float moveSpeed;
+}
+
+public class BattleData : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
