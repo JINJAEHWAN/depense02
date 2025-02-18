4fb2328dedace44cb6eba052f758fafcbd6a056d
tokimili <81837305+tokimili@users.noreply.github.com>
2025-02-18 15:54:30 +0900

    Reapply "몬스터 소환 및 이동"
    This reverts commit 6b68f1a3863386437ffcaa3eeec3203916e79581.

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -3,10 +3,33 @@ using UnityEngine;
 
 public class Monster : BattleData
 {
+
+    public enum myState
+    {
+        create, move, battle, hit
+    }
+
+    public myState nowState = myState.create;
+
+    void changeState(myState s)
+    {
+        if (nowState == s) return;
+        switch (s)
+        {
+            case myState.create:
+                setState();
+                myStateCo = StartCoroutine(move());
+            break;
+        }
+    }
+
+    Coroutine myStateCo;
+    public LayerMask CrashMask;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(move());
+        changeState(myState.create);
     }
 
     // Update is called once per frame
@@ -21,8 +44,26 @@ public class Monster : BattleData
         transform.parent = null;
         while (true)
         {
-            transform.position *= delta * Vector2.left ;
+            transform.Translate(delta * Vector3.left*2.0f) ;
             yield return null;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((1 << collision.gameObject.layer & CrashMask) != 0)
+        {
+            StopCoroutine(myStateCo);
+            //StopAllCoroutines();
+            //StartCoroutine(Battle());
+        }
+    }
+    void setState()
+    {
+        //data.hp = data.hp * StageLevel;
+        //data.MaxHp;
+        //data.attackPower;
+        //data.attackSpeed;
+        //data.cost;
     }
 }
