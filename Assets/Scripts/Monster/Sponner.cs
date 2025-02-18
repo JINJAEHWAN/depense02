79148beee6e83480ee2cdbf45d7632ad27859baf
tokimili <81837305+tokimili@users.noreply.github.com>
2025-02-18 15:51:58 +0900

    몬스터 소환 및 이동
    몬스토 소환 및 이동 작업물 입니다. 몬스터 이동 시 선택한 레이어의 object를 만나게 되면 멈추도록 되어 있습니다.

--- Assets/Scripts/Monster/Sponner.cs
+++ Assets/Scripts/Monster/Sponner.cs
@@ -1,29 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 
-public class Sponner : MonoBehaviour
+public class Sponner : StageLevel
 {
     /// <summary>
     /// Spawner가 맞음....
     /// </summary>
     [Header("스포너 위치에 따른 생성")]
-    public Transform[] Sponners;
+    [SerializeField] Vector2[] Sponners;
 
     [Header("소환 할 유닛")]
-    public GameObject[] Monster;
+    [SerializeField] Monster[] Monster;
 
+    [SerializeField] LayerMask layerMask;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject summon = Instantiate(Monster[Random.Range(0,2)],Sponners[Random.Range(0, 4)].transform.position, Quaternion.identity);
-        Debug.Log(Monster[Random.Range(0, 2)]);
-        Debug.Log(Sponners[Random.Range(0, 4)]);
+        StartCoroutine(summonMonster());
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    IEnumerator summonMonster()
+    {
+        while(true)
+        {
+            Vector2 rndPosition = Sponners[Random.Range(0, Sponners.Length)];
+            Monster un = Instantiate(Monster[Random.Range(0,Monster.Length)], new Vector2(rndPosition.x,rndPosition.y), Quaternion.identity);
+            un.CrashMask = layerMask;
+            yield return new WaitForSeconds(4.0f);
+        }
     }
 }
