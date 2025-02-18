6b68f1a3863386437ffcaa3eeec3203916e79581
tokimili <81837305+tokimili@users.noreply.github.com>
2025-02-18 15:53:19 +0900

    Revert "몬스터 소환 및 이동"
    This reverts commit 79148beee6e83480ee2cdbf45d7632ad27859baf.

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -3,33 +3,10 @@ using UnityEngine;
 
 public class Monster : BattleData
 {
-
-    public enum myState
-    {
-        create, move, battle, hit
-    }
-
-    public myState nowState = myState.create;
-
-    void changeState(myState s)
-    {
-        if (nowState == s) return;
-        switch (s)
-        {
-            case myState.create:
-                setState();
-                myStateCo = StartCoroutine(move());
-            break;
-        }
-    }
-
-    Coroutine myStateCo;
-    public LayerMask CrashMask;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        changeState(myState.create);
+        StartCoroutine(move());
     }
 
     // Update is called once per frame
@@ -44,26 +21,8 @@ public class Monster : BattleData
         transform.parent = null;
         while (true)
         {
-            transform.Translate(delta * Vector3.left*2.0f) ;
+            transform.position *= delta * Vector2.left ;
             yield return null;
         }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if ((1 << collision.gameObject.layer & CrashMask) != 0)
-        {
-            StopCoroutine(myStateCo);
-            //StopAllCoroutines();
-            //StartCoroutine(Battle());
-        }
-    }
-    void setState()
-    {
-        //data.hp = data.hp * StageLevel;
-        //data.MaxHp;
-        //data.attackPower;
-        //data.attackSpeed;
-        //data.cost;
     }
 }
--- Assets/Scripts/Monster/Sponner.cs
+++ Assets/Scripts/Monster/Sponner.cs
@@ -1,41 +1,29 @@
-using System.Collections;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 
-public class Sponner : StageLevel
+public class Sponner : MonoBehaviour
 {
     /// <summary>
     /// Spawner가 맞음....
     /// </summary>
     [Header("스포너 위치에 따른 생성")]
-    [SerializeField] Vector2[] Sponners;
+    public Transform[] Sponners;
 
     [Header("소환 할 유닛")]
-    [SerializeField] Monster[] Monster;
+    public GameObject[] Monster;
 
-    [SerializeField] LayerMask layerMask;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(summonMonster());
+        GameObject summon = Instantiate(Monster[Random.Range(0,2)],Sponners[Random.Range(0, 4)].transform.position, Quaternion.identity);
+        Debug.Log(Monster[Random.Range(0, 2)]);
+        Debug.Log(Sponners[Random.Range(0, 4)]);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    IEnumerator summonMonster()
-    {
-        while(true)
-        {
-            Vector2 rndPosition = Sponners[Random.Range(0, Sponners.Length)];
-            Monster un = Instantiate(Monster[Random.Range(0,Monster.Length)], new Vector2(rndPosition.x,rndPosition.y), Quaternion.identity);
-            un.CrashMask = layerMask;
-            yield return new WaitForSeconds(4.0f);
-        }
     }
 }
