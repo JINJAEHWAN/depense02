02219c44c83c3c136ae31c2d2a4dde6dc123f299
Dp투데커 <jm40108@gmail.com>
2025-02-19 13:07:27 +0900

    Observer Pattern
    Applied Observer Pattern in Slider,
    Food and Mana Recovery System changed.

--- Assets/Scripts/UI/SliderValueChange.cs
+++ Assets/Scripts/UI/SliderValueChange.cs
@@ -11,20 +11,33 @@ public class SliderValueChange : MonoBehaviour
     [SerializeField] Slider playerHPSliderTop, playerHPSliderField;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Start_Func()
     {
-        
+        FoodSliderValueChange();
+        ManaSliderValueChange();
+        PlayerHPSliderValueChange();
     }
 
     // Update is called once per frame
     void Update()
     {
-        FoodSlider.value = Player.Instance.CurFood / Player.Instance.MaxFood;
-        ManaSlider.value = Player.Instance.CurMana / Player.Instance.MaxMana;
-        FoodText.text = Player.Instance.CurFood.ToString("F0") + " / " + Player.Instance.MaxFood.ToString("F0");
-        ManaText.text = Player.Instance.CurMana.ToString("F0") + " / " + Player.Instance.MaxMana.ToString("F0");
-        playerHPSliderField.value = playerHPSliderTop.value = (float)Player.Instance.data.hp / Player.Instance.data.MaxHp;
         playerHPSliderField.transform.position = Camera.main.WorldToScreenPoint(Player.Instance.hpbarposition.position);
+    }
 
+    public void FoodSliderValueChange()
+    {
+        FoodSlider.value = Player.Instance.CurFood / Player.Instance.MaxFood;
+        FoodText.text = Player.Instance.CurFood.ToString("F0") + " / " + Player.Instance.MaxFood.ToString("F0");
+
+    }
+    public void ManaSliderValueChange()
+    {
+        ManaSlider.value = Player.Instance.CurMana / Player.Instance.MaxMana;
+        ManaText.text = Player.Instance.CurMana.ToString("F0") + " / " + Player.Instance.MaxMana.ToString("F0");
+    }
+    //플레이어 HP 변동할 때마다 갖다 쓰시오.
+    public void PlayerHPSliderValueChange()
+    {
+        playerHPSliderField.value = playerHPSliderTop.value = (float)Player.Instance.data.hp / Player.Instance.data.MaxHp;
     }
 }
